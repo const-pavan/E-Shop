0944989fd523d85f12731146592a82d659a1e1df
Pavan <pansgami@gmail.com>
2022-05-15 11:33:43 +0530

    all thing added

---
+++ frontend/src/components/SearchBox.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+
+const SearchBox = ({ history }) => {
+  const [keyword, setKeyword] = useState('')
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if (keyword.trim()) {
+      history.push(`/search/${keyword}`)
+    } else {
+      history.push('/')
+    }
+  }
+
+  return (
+    <Form onSubmit={submitHandler} className='d-flex' inline>
+      <Form.Control
+        type='text'
+        name='q'
+        onChange={(e) => setKeyword(e.target.value)}
+        placeholder='Search Products...'
+        className='form-control me-sm-2'
+      ></Form.Control>
+      <Button
+        type='submit'
+        variant='outline-success'
+        className='btn btn-secondary my-2 my-sm-0'
+      >
+        Search
+      </Button>
+    </Form>
+  )
+}
+
+export default SearchBox
